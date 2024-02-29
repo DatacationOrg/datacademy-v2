862aff197fb95c9a32b95bff9b872ca184467c70
Remco Coppens <remcocoppens@outlook.com>
2024-02-29 09:43:08 +0100

    Cleaned Exercise Files

--- M06_APIA/main.py
+++ M06_APIA/main.py
@@ -12,15 +12,18 @@ services.create_database()
 def create_user(user: schemas.UserCreate, db: orm.Session=fastapi.Depends(services.get_db)):
     ...
 
+
 #TODO: Complete the API function to retrieve user information based on a skip and limit parameter.
 @app.get("/users/", response_model=list[schemas.User])
 def read_users(skip: int = 0, limit: int = 10, db: orm.Session = fastapi.Depends(services.get_db)):
     ...
 
+
 #TODO: Complete the API function to retrieve user information based on a specific user_id.
 @app.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: orm.Session = fastapi.Depends(services.get_db)):
     ...
+
 
 #TODO: Complete the API function to create an article for a user.
 @app.post("/users/{user_id}/posts/", response_model=schemas.Article)
@@ -28,20 +31,24 @@ def create_article_for_user(user_id: int, article: schemas.ArticleCreate,
                             db: orm.Session = fastapi.Depends(services.get_db)):
     ...
 
+
 #TODO: Complete the API function to retrieve article information based on a skip and limit parameter.
 @app.get("/posts/", response_model=list[schemas.Article])
 def read_articles(skip: int = 0, limit: int = 10, db: orm.Session = fastapi.Depends(services.get_db)):
     ...
+
 
 #TODO: Complete the API function to retrieve article information based on a specific article_id.
 @app.get("/posts/{article_id}", response_model=schemas.Article)
 def read_article(article_id: int, db: orm.Session = fastapi.Depends(services.get_db)):
     ...
 
+
 #TODO: Complete the API function to delete an article based on a specific article_id.
 @app.delete("/posts/{article_id}")
 def delete_article(article_id: int, db: orm.Session = fastapi.Depends(services.get_db)):
     ...
+
 
 #TODO: Complete the API function to change the information of a given article based on a specific article_id.
 @app.put("/posts/{article_id}")
--- M06_APIA/schemas.py
+++ M06_APIA/schemas.py
@@ -4,17 +4,22 @@ import pydantic
 #TODO: Develop the "_ArticleBase" class.
 ...
 
+
 #TODO: Develop the "ArticleCreate" class.
 ...
+
 
 #TODO: Develop the "Article" class using the "_ArticleBase" class.
 ...
 
+
 #TODO: Develop the "_UserBase" class.
 ...
 
+
 #TODO: Develop the "UserCreate" class using the "_UserBase" class.
 ...
+
 
 #TODO: Develop the "User" class using the "_UserBase" class.
 ...
